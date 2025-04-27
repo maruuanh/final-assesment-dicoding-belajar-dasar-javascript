a0f19719d3f6d897d9216519e5abbf83587f48fa
Marwan Hadid <marwanhadid6@gmail.com>
2025-04-27 19:56:30 +0700

    update

--- 03-writing-test/index.test.js
+++ 03-writing-test/index.test.js
@@ -1,3 +1,7 @@
 import assert from 'node:assert';
+import {test} from 'node:test';
 import sum from './index.js';
-assert.equal(sum(2,3), 5);
+
+test('Result must be 5', () => {
+    assert.equal(sum(2,3), 5);
+});
